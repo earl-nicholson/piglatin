3abcfc4fd8d2fe6208bde1707e0e188cd9f438d1
earl-nicholson <earl.r.nicholson@gmail.com>
2022-10-11 15:48:29 -0400

    updated preloaded text

--- src/App.js
+++ src/App.js
@@ -9,9 +9,9 @@ class App extends Component {
     this.state = {
       // "phrase" is the text entered by the user - right now there are test words hard coded to make the process of testing your code faster and easier
       // ACTION ITEM: when you are ready for your full user experience, delete the test words so phrase is assigned an empty string
-      phrase: "alpha through yummy squeal queen fry",
+      phrase: "Type Here To See Conversion Below",
       // "phraseTranslated" is what the user will see appear on the page as Pig Latin, it starts as the preset message and updates when your user clicks the "submit" button
-      phraseTranslated: "This is where your translated sentence will appear."
+      phraseTranslated: "Type Here To See Conversion Below"
     }
   }
 
@@ -69,7 +69,7 @@ class App extends Component {
     // this method restarts the game by setting the original state
     // ACTION ITEM: when you are ready for your full user experience, delete the test words in phrase so that is assigned an empty string
     this.setState({
-      phrase: "alpha through yummy squeal queen fry",
+      phrase: "Type Here To See Conversion Below",
       phraseTranslated: "This is where your translated sentence will appear."
     })
   }
